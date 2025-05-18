143f23896835dcdb49edaa377b6f8320d5dda5e6
Adeline <adeline.patenne@hotmail.fr>
2025-05-18 20:55:06 +0200

    refactor: change all int into hand enum using less static_cast

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 int main(int argc, char const *argv[])
 {
     std::setlocale(LC_ALL, ".utf8");
-    auto choice{0};
-    auto random_value{-1};
-    auto bot_score{0};
-    auto user_score{0};
-    auto number_turn{0};
+    int choice{0};
+    hand random_value{hand::paper};
+    int bot_score{0};
+    int user_score{0};
+    int number_turn{0};
 
     do {
         number_turn++;
@@ -17,13 +17,12 @@ int main(int argc, char const *argv[])
             cout<<"Entrée invalide, veuillez recommencer !"<<endl<<endl;
             continue;
         } else if (choice != 0) {
-            // choice--;
             random_value = getRandomNumber();
-            calculateScore(choice-1, random_value, user_score, bot_score);
-            displayChoice(choice-1, random_value);
+            calculateScore(static_cast<hand>(choice-1), random_value, user_score, bot_score);
+            displayChoice(static_cast<hand>(choice-1), random_value);
             displayResult(user_score, bot_score, number_turn);
-            // choice++;
         }
+        
     } while (choice != 0);
     cout<<"Merci d'avoir joué ! Bye Bye ..."<<endl;
     return 0;
@@ -42,7 +41,7 @@ int displayMenu() {
     return choice;
 }
 
-void displayChoice (const int choice, const int random_value) {
+void displayChoice (const hand choice, const hand random_value) {
     string user_string = setString(choice);
     string bot_string = setString(random_value);
     cout<<"Vous avez choisi "<<user_string<<endl;
@@ -52,11 +51,11 @@ void displayChoice (const int choice, const int random_value) {
 /// @brief Generate string "rock" "paper" "scissors" depending on the int choice made by user or bot
 /// @param choice int
 /// @return Rock Paper or Scissors
-string setString(const int choice) {
+string setString(const hand choice) {
     string final_string = "";
-    if (choice == static_cast<int>(hand::rock)){
+    if (choice == hand::rock){
         final_string = str_rock;
-    } else if (choice == static_cast<int>(hand::paper)) {
+    } else if (choice == hand::paper) {
         final_string = str_paper;
     } else {
         final_string = str_scissors;
@@ -64,37 +63,37 @@ string setString(const int choice) {
     return final_string;
 }
 
-int getRandomNumber() {
+hand getRandomNumber() {
     std::srand(time(nullptr)); //get the value to shuffle the random number
     auto random_value {std::rand()};
-    return random_value%3;
+    return static_cast<hand>(random_value%3);
 }
 
 /// @brief compare the choice and randomValue to increment user_score or bot_score
-/// @param choice user_choice from menu
+/// @param choice user_choice from menu cast into hand
 /// @param random_value value from getRandomNumber AKA bot choice
 /// @param user_score 
 /// @param bot_score 
-void calculateScore(const int choice, const int random_value, int& user_score, int& bot_score) {
+void calculateScore(const hand choice, const hand random_value, int& user_score, int& bot_score) {
     switch (choice) {
-        case static_cast<int>(hand::rock):
-            if (random_value == static_cast<int>(hand::scissors)) {
+        case hand::rock:
+            if (random_value == hand::scissors) {
                 user_score++;     
-            } else if (random_value == static_cast<int>(hand::paper)) {
+            } else if (random_value == hand::paper) {
                 bot_score++;
             }
             break;
-        case static_cast<int>(hand::paper):
-            if (random_value == static_cast<int>(hand::rock)) {
+        case hand::paper:
+            if (random_value == hand::rock) {
                 user_score++;     
-            } else if (random_value == static_cast<int>(hand::scissors)) {
+            } else if (random_value == hand::scissors) {
                 bot_score++;
             }
             break;
-        case static_cast<int>(hand::scissors):
-            if (random_value == static_cast<int>(hand::paper)) {
+        case hand::scissors:
+            if (random_value == hand::paper) {
                 user_score++;     
-            } else if (random_value == static_cast<int>(hand::rock)) {
+            } else if (random_value == hand::rock) {
                 bot_score++;
             }
             break;
